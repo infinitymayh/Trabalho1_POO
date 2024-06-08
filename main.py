5d63993519e2719261e553782f95c56d5df9e11b
Mayara <mayarasoares@usp.br>
2024-06-07 21:43:09 -0300

    mudanças na main

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ help_message = ("\n \nUsage: main.py [-h] {list,task,new} ... \n \n"
 				"Manage your tasks in the command line!\n \n"
 				"Options: -h, --help  show this help message and exit\n \n"
 				"Commands: {list,task,new}\n \n")
-task_list = [""]
-list_message = ("Task list:\n"
-"================================================================================\n"
-"   ID Description                                      State     Pri. Date      \n"
-+task_list[0]+
-"\n--------------------------------------------------------------------------------\n"
-"================================================================================\n")
+task_list = {}
+list_message = ["Task list:\n"
+				"================================================================================\n"
+				"   ID Description                                      State     Pri. Date      \n", *task_list,
+				"\n--------------------------------------------------------------------------------\n"
+				"================================================================================\n"]
+list_help = ("\n \nUsage: todo.py list [-h] [-a] [-p {1,2,3}]\n \n"
+			"List the tasks\n \n"
+			"Options:\n"
+			"  -h, --help                     Show this help message and exit\n"
+			"  -a, --all                      List all tasks, not only pendent\n"
+			"  -p {1,2,3}, --priority {1,2,3} Show tasks with a given prioriy\n \n")
 
 # -> verifica se foi executado apenas o "main.py", nesse caso é mandada
 #uma mensagem de inicialização
@@ -35,16 +40,11 @@ else:
 	
 	elif sys.argv[1] == "list":
 		if len(sys.argv) == 2:
-			print("list of pending tasks")
+			print(*list_message)
 		
 		else:
 			if sys.argv[2] == "-h" or sys.argv[2] == "--help":
-				print("\n \nUsage: todo.py list [-h] [-a] [-p {1,2,3}]\n \n"
-					"List the tasks\n \n"
-					"Options:\n"
-					"  -h, --help                     Show this help message and exit\n"
-					"  -a, --all                      List all tasks, not only pendent\n"
-					"  -p {1,2,3}, --priority {1,2,3} Show tasks with a given prioriy\n \n")
+				print(list_help)
 				exit()
 			
 			elif sys.argv[2] == "-a" or sys.argv[2] == "--all":
