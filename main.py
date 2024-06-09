396e393793267458581f19c9b6b0e1ed5c65e600
Kevyn115 <kevyn.reis@usp.br>
2024-06-09 13:16:55 -0300

    comando new

--- main.py
+++ main.py
@@ -14,17 +14,13 @@ list_help = ("\n \nUsage: todo.py list [-h] [-a] [-p {1,2,3}]\n \n"
 			"  -h, --help                     Show this help message and exit\n"
 			"  -a, --all                      List all tasks, not only pendent\n"
 			"  -p {1,2,3}, --priority {1,2,3} Show tasks with a given prioriy\n \n")
-list_help2 = ("./todo.py new -h usage: todo.py new [-h] description"
-			"\nCreate a task"
-			"\npositional arguments: description Create a new task"
-			"\noptions: -h, --help show this help message and exit")
+			
+list_help2 = ("\n\n./todo.py new -h usage: todo.py new [-h] description"
+			"\n\nCreate a task"
+			"\n\npositional arguments:\n  -description Create a new task"
+			"\n\noptions:\n  -h, --help show this help message and exit")
 
 
-
-
-
-tasklist = task_list()
-tasksid = 0
 
 # -> verifica se foi executado apenas o "main.py", nesse caso é mandada
 #uma mensagem de inicialização
@@ -46,7 +42,7 @@ else:
 	
 	elif sys.argv[1] == "list":
 		if len(sys.argv) == 2:
-			tasklist.msg_pending()
+			msg_pending()
 		
 		else:
 			if sys.argv[2] == "-h" or sys.argv[2] == "--help":
@@ -63,13 +59,33 @@ else:
 				
 	elif sys.argv[1] == "new":
 		
+		#comando de ajuda
+		
 		if sys.argv[2] == "-h" or sys.argv[2] == "--help":
 				print(list_help2)
 				
+		#criando uma nova tarefa
+		
 		else:
-			tarefa_nova = task(tasksid, sys.argv[2])
-			tasksid += 1
-			tasklist._tasks.append(tarefa_nova)
-			print(tarefa_nova._description)
-			print(tasklist._tasks)
-			print(tasksid)
+			nova_tarefa=sys.argv[2]
+			tarefa,estado=carregar_tarefas(nome_arquivo)
+			
+			# caso não exista uma lista de tarefas
+			
+			if not tarefa:
+				tarefas = {}
+				estado = {}
+
+			# criação de id usando loop	
+
+			n_id = 0
+			for i in (tarefa):
+				n_id= n_id + 1
+
+			# salvar tarefas no arquivo
+
+			tarefa[n_id]=nova_tarefa
+			estado[n_id]="pending"
+			salvar_tarefas(tarefa,estado, nome_arquivo)
+
+
--- objects.py
+++ objects.py
@@ -10,30 +10,55 @@ class task:
 			self.priority = p
 		return self.priority
 
-class task_list:
-	def __init__(self):
-		self._tasks = []
-		
-		self.list_message = ["Task list:\n"
+
+list_message = ["Task list:\n"
 				"================================================================================\n"
 				"   ID Description                                      State     Pri. Date      \n",
 				"\n--------------------------------------------------------------------------------\n"
 				"================================================================================\n"]
 	
-	def msg_pending(self):
-		pending_list = []
-		for i in self._tasks:
-			if i._state == "pending":
-				pending_list.append(i)
-		print(self.list_message[0])
-		for i in pending_list:
-			print("   ",i._id, i._description+(49-len(i._description))*" "+i._state,"  ",i.priority)
-		print(self.list_message[1])
+def msg_pending():
+	tarefa,estado=carregar_tarefas(nome_arquivo)
+	print(list_message[0])
+	for i in estado:
+		if estado[i] == "pending":
+			contagem = 0
+			for j in tarefa[i]:
+				contagem += 1
+			print("   ",i,tarefa[i]," "*(47-contagem),estado[i],"  ",)
+	print(list_message[1])
 			
 	
 	def msg(self):
 		return str(self.list_message[0]+'\n'.join(i for i in self._tasks)+self.list_message[1])
 		
 		
+#=================================================================================================================
 
-		
+# salvar as tarefas
+
+import json
+
+#nome do arquivo das tarefas
+
+nome_arquivo = 'tarefa.json'
+
+
+# funcao para abrir o arquvivo das tarefas
+
+def carregar_tarefas(nome_arquivo):
+	try:
+		with open(nome_arquivo,'r') as arquivo:
+			dic = json.load(arquivo)
+			if dic != 0:
+				return dic["tarefas"],dic["estado"]
+	except FileNotFoundError:
+		return {},{}
+        
+#funcao para salvar as tarefas no arquivo
+
+def salvar_tarefas(tarefas,estado, nome_arquivo):
+    with open(nome_arquivo, 'w') as arquivo:
+        json.dump({"tarefas": tarefas, "estado": estado}, arquivo)
+        
+
